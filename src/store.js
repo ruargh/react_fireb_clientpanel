1b21b644bcf469b30e76d5307b06b5c255f25c27
Ruargh <ruargh@gmail.com>
2018-11-02 18:14:25 +0200

    10. Client Panel Firebase Project - Part 1 | 5. React-Redux-Firebase Setup

---
+++ src/store.js
@@ -0,0 +1,53 @@
+import { createStore, combineReducers, compose } from 'redux';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
+import { reduxFirestore, firestoreReducer } from 'redux-firestore';
+//Reducers
+// @todo
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBgiitLMcBllDVCHGQLKgw-hCA-oYOEr_A',
+  authDomain: 'reactclientpanel-eee5b.firebaseapp.com',
+  databaseURL: 'https://reactclientpanel-eee5b.firebaseio.com',
+  projectId: 'reactclientpanel-eee5b',
+  storageBucket: 'reactclientpanel-eee5b.appspot.com',
+  messagingSenderId: '473880132135'
+};
+
+//React-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true
+};
+
+//Init firebase instance
+firebase.initializeApp(firebaseConfig);
+//Init firestore
+//const firestore = firebase.firestore();
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig), // firebase instance as first argument
+  reduxFirestore(firebase)
+)(createStore);
+
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer
+});
+
+// Create initial state
+const initialState = {};
+
+// Create store
+const store = createStoreWithFirebase(
+  rootReducer,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
