034e9d2a8c00782e207bbacdbba6497c9eb2487d
Ruargh <ruargh@gmail.com>
2018-11-06 12:01:22 +0200

    10. Client Panel Firebase Project - Part 1 | 7. Add Client To Firestore

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
-import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 
 class Clients extends Component {
